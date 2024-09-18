4afc592136e0509010df2715d92a7e37e6f75957
Don Bosco <don965877@gmail.com>
2024-09-19 02:12:40 +0300

    first react exercise

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+import Food from './components/Food'
+import Footer from './components/Footer'
+
+function App() {
+  return(
+    <>
+    <Header/>
+    <Food/>
+    <Footer/>
+    </>
+  )
+}
+export default App;
---
+++ my-react-app/src/components/Food.jsx
@@ -0,0 +1,17 @@
+
+
+function Food() {
+
+    const food1 = "Natasha";
+    const food2 = "Moneyyy";
+    return(
+  <ul>
+    <li>Bread</li>
+    <li>{food1}</li>
+    <li>{food2}</li>
+    <li>Cars</li>
+  </ul>
+    );
+}
+
+export default Food;
